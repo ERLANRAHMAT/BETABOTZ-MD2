abc7e595f4ea9a0663a9064ee1bfd37b62dd7225
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-06-13 15:53:32 +0700

    Update _openai-ai.js

--- plugins/_openai-ai.js
+++ plugins/_openai-ai.js
@@ -1,16 +1,31 @@
 var fetch = require('node-fetch');
+
 var handler = async (m, {
+
  text, 
+
  usedPrefix, 
+
  command
+
  }) => {
+
 if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia? `
+
 await m.reply(wait)
-  var apii = await fetch(`https://botcahx.vercel.app/openai?text=${text}`)
-  var js = await apii.json()
-  await m.reply(js.result)
+
+  var js = await fetch(API('lann', '/api/search/openai-chat', { text: `${text}`, apikey: lann }))
+
+var json = await js.json()
+
+  await m.reply(json.message)
+
 }      
+
 handler.command = handler.help = ['ai','openai','chatgpt'];
+
 handler.tags = ['info'];
+
 handler.premium = false
+
 module.exports = handler;
