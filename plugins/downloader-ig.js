cac3031251605219b812a51747ff66da0f367c53
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-06-14 12:12:55 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
 
-   const res = await instagram.v1(args[0)])
+   const res = await instagram.v1(args[0])
 
 for (let i of res) {
 
