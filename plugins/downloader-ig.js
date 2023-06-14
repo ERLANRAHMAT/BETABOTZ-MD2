ba4ce6f5fd919fb11d3d30a9a169e51a096ed53d
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-06-14 13:05:47 +0700

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,12 +1,18 @@
 const fetch = require("node-fetch")
+const util = require("util")
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.instagram.com/p/ByxKbUSnubS/?utm_source=ig_web_copy_link`
-   const url = args[0];
-   const re = await fetch(API('lann', '/api/download/igdowloader', { url: url, apikey: lann }))
-   let message = await re.json()
-   for (let i of message.message.url ) {
-    conn.sendFile(m.chat, i, null, `*Instagram Downloader*`, m)
+    m.reply(wait)
+     const url = args[0];
+     let re = await fetch(API('lann', '/api/download/igdowloader', { url: url, apikey: lann }))
+     let message = await re.json()  
+    try {             
+        for (let i of message.message.url ) {
+            conn.sendFile(m.chat, i, null, `*Instagram Downloader*`, m)
+        }
+    } catch(err) {
+        m.reply(util.format(message))
     }
 }
 
@@ -14,4 +20,5 @@ handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(Instagram|ig|igdl|igstory)$/i
 handler.limit = true
+
 module.exports = handler
