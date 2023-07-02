c7901b184da45b7bda9aaad5315cef676d445d54
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-07-02 14:16:29 +0700

    Update simii

--- plugins/fun-simsimi.js
+++ plugins/fun-simsimi.js
@@ -1,13 +1,11 @@
 let fetch = require('node-fetch')
 let handler = async (m, { text }) => {
-  let res = await fetch(global.API('https://api.simsimi.net', '/v2/', { text: encodeURIComponent(text), lc: "id" }, ''))
+  let res = await fetch(`http://api.brainshop.ai/get?bid=175444&key=DxrtWTGGOUvgKqya&uid=175444&msg=${encodeURIComponent(text)}`)
   let json = await res.json()
-  if (json.success) m.reply(json.success)
-  else throw json
+  m.reply(json.cnt)
 }
 handler.help = ['simi', 'simsimi', 'simih'].map(v => v + ' <teks>')
 handler.tags = ['fun']
 handler.command = /^((sim)?simi|simih)$/i
 
 module.exports = handler
-
