3e7c95c48a4b29c99f1137930af3c931361f5ee5
Nayla Hanifah <138402457+Nayla-Hanifah@users.noreply.github.com>
2023-08-12 23:05:17 +0700

    Create _allfake.js

---
+++ plugins/_allfake.js
@@ -0,0 +1,77 @@
+let fs = require('fs')
+let fetch = require('node-fetch')
+const moment = require('moment-timezone')
+
+let handler = m => m
+handler.all = async function (m) {
+	let pp = 'https://i.ibb.co/gS0XrNc/avatar-contact.png'
+	try {
+		pp = await this.profilePictureUrl(m.sender, 'image')
+	} catch (e) {
+	} finally {
+		
+        global.bg = await (await fetch(thumb)).buffer()
+		global.doc = pickRandom(["application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/msword", "application/pdf"])
+		
+		// Module 
+		global.fetch = require('node-fetch')
+		
+		const _uptime = process.uptime() * 1000
+        
+		// Ini untuk command crator/owner
+		global.kontak2 = [
+         [owner[0], await this.getName(owner[0] + '@s.whatsapp.net'), 'ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ', 'Nothing', true],
+         [owner[1], await this.getName(owner[1] + '@s.whatsapp.net'), 'ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ', 'Nothing', true], // Kalo mau di tambah tinggal copy 1baris ini di tempel di bawahnya trs di edit dikit!
+        ]
+        
+		// ucapan ini mah
+		global.ucapan = ucapan()
+		global.wib = moment.tz('Asia/Jakarta').format("HH:mm:ss")
+		// pesan sementara
+		global.ephemeral = '86400' // 86400 = 24jam, kalo ingin di hilangkan ganti '86400' jadi 'null' atau ''
+		
+		// externalAdReply atau text with thumbnail. gatau bahasa Inggris? coba translate!
+		global.adReply = {
+			contextInfo: {
+				forwardingScore: 9999,
+				isForwarded: true, // ini biar ada tulisannya diteruskan berkali-kali, jika ingin di hilangkan ganti true menjadi false
+				externalAdReply: { // Bagian ini sesuka kalian berkreasi :'v
+					title: 'Time: ' + global.wib,
+					body: wm,
+					previewType: "PHOTO",
+					thumbnail: await (await fetch(pp)).buffer(),
+					sourceUrl: 'https://wa.me',
+				}
+			}
+		}
+		
+		// Fake 🤥
+		global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
+		global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./media/thumbnail..jpg'), thumbnail: require('fs').readFileSync('./media/thumbnail.jpg'),sendEphemeral: true}}}
+		
+	}
+}
+
+module.exports = handler
+
+function ucapan() {
+    const time = moment.tz('Asia/Jakarta').format('HH')
+    let res = "Selamat malam 🌙"
+    if (time >= 4) {
+        res = "Selamat pagi 🌄"
+    }
+    if (time > 10) {
+        res = "Selamat siang ☀️"
+    }
+    if (time >= 15) {
+        res = "Selamat sore 🌅"
+    }
+    if (time >= 18) {
+        res = "Selamat malam 🌙"
+    }
+    return res
+}
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
