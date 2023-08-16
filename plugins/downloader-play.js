b853f06a1ab79c102b27dd8f42565f5d9e246b18
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-08-16 18:03:01 +0700

    Update youtube play

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,76 +1,73 @@
-var { youtubeSearch } = require('@bochilteam/scraper');
-   var handler = async (m, { 
+const uploadImage = require('../lib/uploadImage')
+const fetch = require('node-fetch')
+const youtube = require("yt-search");
+
+var handler = async (m, {
     conn,
-    text, 
+    text,
     usedPrefix
-               }) => {
-  if (!text) throw 'Enter Title / link'
-  try {
-    var vid = (await youtubeSearch(text)).video[0]
-    if (!vid) throw 'Video/Audio Tidak Ditemukan'
-    var { title, 
-          description, 
-          thumbnail, 
-          videoId, 
-          durationH, 
-          durationS,
-          viewH,
-          publishedTime
-                         } = vid
-    var url = 'https://www.youtube.com/watch?v=' + videoId
+}) => {
+    if (!text) throw 'Enter Title / Link From YouTube!';
+    try {
+        var search = await youtube(text);
+        var convert = search.videos[0];
+        if (!convert) throw 'Video/Audio Tidak Ditemukan';
+        if (convert.seconds >= 3600) {
+            return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
+        } else {
+            var audioUrl
+            try {
+                audioUrl = `https://yt.tioo.eu.org/?url=${convert.url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+            } catch (e) {
+                conn.reply(m.chat, wait, m)
+                audioUrl = `https://yt.tioo.eu.org/?url=${convert.url}&filter=audioonly&quality=highestaudio&contenttype=audio/mpeg`
+            } 
+            var build = await fetch(convert.image);
+            var buffer = await build.buffer();
+            var image = await uploadImage(buffer);
+            var caption = `∘ Title : ${convert.title}\n∘ Ext : Search\n∘ ID : ${convert.videoId}\n∘ Duration : ${convert.timestamp}\n∘ Viewers : ${convert.views}\n∘ Upload At : ${convert.ago}\n∘ Author : ${convert.author.name}\n∘ Channel : ${convert.author.url}\n∘ Url : ${convert.url}\n∘ Description : ${convert.description}\n∘ Thumbnail : ${image}`;
+            var pesan = conn.relayMessage(m.chat, {
+                extendedTextMessage:{
+                text: caption, 
+                contextInfo: {
+                     externalAdReply: {
+                        title: "Powered by",
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: image,
+                        sourceUrl: audioUrl
+                    }
+                }, mentions: [m.sender]
+                }}, {})
+            conn.sendMessage(m.chat, {
+                audio: {
+                    url: audioUrl
+                },
+                mimetype: 'audio/mpeg',
+                contextInfo: {
+                    externalAdReply: {
+                        title: convert.title,
+                        body: "",
+                        thumbnailUrl: image,
+                        sourceUrl: audioUrl,
+                        mediaType: 1,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true
+                    }
+                }
+            }, {
+                quoted: m
+            });
+        }
+    } catch (e) {
+        conn.reply(m.chat, `*Error:* ` + e, m);
+    }
+};
 
-   let vide = `https://yt.btch.bz/download?URL=${url}&videoName=video`
-
-    let web = `https://yt.btch.bz/downloadAudio?URL=${url}&videoName=video`
-    var tmb = thumbnail
-    var captionvid = `  ∘ Title: ${title}
-  ∘ Published: ${publishedTime}
-  ∘ Duration: ${durationH}
-  ∘ Second: ${durationS}
-  ∘ Views: ${viewH}  
-  ∘ Url:  ${url}
-  ∘ Description: ${description}`
-    var pesan = await conn.sendMessage(m.chat, {
-    text: captionvid,
-    contextInfo: {
-    externalAdReply: {
-    title: "",
-    body: "Powered by",
-    thumbnailUrl: tmb ,
-    sourceUrl: web,
-    mediaType: 1,
-    showAdAttribution: true,
-    renderLargerThumbnail: true
-    }}})
-
-    if (durationS > 18000) return conn.sendMessage(m.chat, { text: `*Link Original:* ${await cut(url)}\n\n_Durasi terlalu panjang..._\n*Duration Limit!*` }, { quoted: pesan })
-    conn.sendMessage(m.chat, { audio: { url: web }, mimetype: 'audio/mpeg', contextInfo: {
-    externalAdReply: {
-    title: title,
-    body: "",
-    thumbnailUrl: tmb,
-    sourceUrl: web,
-    mediaType: 1,
-    showAdAttribution: true,
-    renderLargerThumbnail: true
-    }}} , { quoted: pesan })
-
-  } catch (e) {
-    throw 'Video/Audio Tidak Ditemukan'
-  }
-}
-handler.command = handler.help = ['play','song','youtube','ytmp3','ds','downloadyt','yta'];
+handler.command = handler.help = ['play', 'song', 'ds', 'ytmp3', 'yta'];
 handler.tags = ['downloader'];
 handler.exp = 0;
 handler.limit = true;
 handler.premium = false;
-module.exports = handler;
-async function cut(url) {
-  url = encodeURIComponent(url)
-  let res = await fetch(`https://api.botcahx.live/api/linkshort/bitly?link=${url}&apikey=${btc}`)
-  if (!res.ok) throw false
-  return await res.text()
-}
-async function delay(ms) {
-   await new Promise(resolve => setTimeout(resolve, ms));
-}
+module.exports = handler
