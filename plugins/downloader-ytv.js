588c4f6e24165b8e1116ca71d2e8fe6b7964c1b1
Tio <94074076+BOTCAHX@users.noreply.github.com>
2023-08-16 18:01:38 +0700

    Update youtube video

--- plugins/downloader-ytv.js
+++ plugins/downloader-ytv.js
@@ -1,61 +1,49 @@
-var { youtubeSearch } = require('@bochilteam/scraper');
-var handler = async (m, { conn, text, usedPrefix }) => {
-  if (!text) throw 'Enter Title / link'
-  try {
-    var vid = (await youtubeSearch(text)).video[0]
-    if (!vid) throw 'Video/Audio Tidak Ditemukan'
-    var { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
-    var url = 'https://www.youtube.com/watch?v=' + videoId
-
-    let vide = `https://yt.btch.bz/download?URL=${url}&videoName=video`
-    var tmb = thumbnail
-    var captionvid = `  ∘ Title: ${title}
-  ∘ Published: ${publishedTime}
-  ∘ Duration: ${durationH}
-  ∘ Second: ${durationS}
-  ∘ Views: ${viewH}  
-  ∘ Url:  ${url}
-  ∘ Description: ${description}`
-    var pesan = await conn.sendMessage(m.chat, {
-      text: captionvid,
-      contextInfo: {
-        externalAdReply: {
-          title: "",
-          body: "Powered by",
-          thumbnailUrl: tmb ,
-          sourceUrl: vide,
-          mediaType: 1,
-          showAdAttribution: true,
-          renderLargerThumbnail: true
+const youtube = require("yt-search");
+let handler = async (m, {
+    conn,
+    text,
+    usedPrefix
+}) => {
+    if (!text) throw 'Enter Title / Link From YouTube!';
+    try {
+        var search = await youtube(text);
+        var convert = search.videos[0];
+        if (!convert) throw 'Video/Audio Tidak Ditemukan';
+        if (convert.seconds >= 3600) {
+            return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
+        } else {
+            var videoUrl
+            try {
+                videoUrl = `https://yt.tioo.eu.org/?url=${convert.url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+            } catch (e) {
+                conn.reply(m.chat, wait, m)
+                videoUrl = `https://yt.tioo.eu.org/?url=${convert.url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+            }             
+            var caption = `∘ Title : ${convert.title}\n∘ Ext : Search\n∘ ID : ${convert.videoId}\n∘ Duration : ${convert.timestamp}\n∘ Viewers : ${convert.views}\n∘ Upload At : ${convert.ago}\n∘ Author : ${convert.author.name}\n∘ Channel : ${convert.author.url}\n∘ Url : ${convert.url}\n∘ Description : ${convert.description}\n∘ Thumbnail : ${convert.image}`;
+            var pesan = conn.relayMessage(m.chat, {
+                extendedTextMessage:{
+                text: caption, 
+                contextInfo: {
+                     externalAdReply: {
+                        title: "Powered by ",
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: convert.image,
+                        sourceUrl: videoUrl
+                    }
+                }, mentions: [m.sender]
+                }}, {})
+                conn.sendMessage(m.chat, { video: { url: videoUrl }, mimetype: 'video/mp4' }, { quoted: m })
         }
-      }
-    })
-    if (durationS > 18000) return conn.sendMessage(m.chat, { text: `*Link Original:* ${await cut(url)}\n\n_Durasi terlalu panjang..._\n*Duration Limit!*` }, { quoted: pesan })
+    } catch (e) {
+        conn.reply(m.chat, `*Error:* ` + e, m);
+    }
+};
 
-    conn.sendMessage(m.chat, {
-      video: {
-        url: vide,
-        mimetype: 'video/webm',
-        attributes: [
-          {
-            name: 'controls',
-            value: 'true'
-          },
-          {
-            name: 'autoplay',
-            value: 'true'
-          }
-        ]
-      }
-    }, { quoted: pesan })
-  } catch (e) {
-    throw 'Video/Audio Tidak Ditemukan'
-  }
-}
-
-handler.command = handler.help = ['ytmp4', 'ytv']
-handler.tags = ['downloader']
-handler.exp = 0
-handler.limit = true
-handler.premium = false
-module.exports = handler
+handler.command = handler.help = ['ytv','ytmp4'];
+handler.tags = ['downloader'];
+handler.exp = 0;
+handler.limit = true;
+handler.premium = false;
+module.exports = handler;
