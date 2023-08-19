c2b0cbde345a50e431e637c983a870ef7ddb3752
Nayla Hanifah <138402457+Nayla-Hanifah@users.noreply.github.com>
2023-08-19 09:25:29 +0700

    Apa coba

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -39,7 +39,7 @@ handler.all = async function (m) {
 					title: 'Time: ' + global.wib,
 					body: wm,
 					previewType: "PHOTO",
-					thumbnail: await (await fetch(pp)).buffer(),
+					thumbnail: await (await fetch(thumb)).buffer(),
 					sourceUrl: 'https://api.betabotz.org',
 				}
 			}
@@ -47,7 +47,7 @@ handler.all = async function (m) {
 		
 		// Fake 🤥
 		global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
-		global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./media/thumbnail..jpg'), thumbnail: require('fs').readFileSync('./media/thumbnail.jpg'),sendEphemeral: true}}}
+		global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./media/thumbnail.jpg'), thumbnail: require('fs').readFileSync('./media/thumbnail.jpg'),sendEphemeral: true}}}
 		
 	}
 }
