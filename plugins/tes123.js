79159bc60b6bc03c997abb4b20f83ec27d3d4662
Nayla Hanifah <138402457+Nayla-Hanifah@users.noreply.github.com>
2023-08-26 15:51:10 +0700

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,7 +1,5 @@
 let handler = async (m, { conn }) => {
     let wm = global.wm
-    let _uptime = process.uptime() * 1000
-    let uptimex = clockString(_uptime)
     let txt = `*_Aktif Kak ada yang saya bisa bantu\nketik .menu untuk mengakses bot_*`.trim()
 let sgc = global.gc
 conn.sendMessage(m.chat, {
@@ -28,15 +26,4 @@ module.exports = handler
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
-}
-
-function clockString(ms) {
-    let days = Math.floor(ms / (24 * 60 * 60 * 1000));
-    let daysms = ms % (24 * 60 * 60 * 1000);
-    let hours = Math.floor((daysms) / (60 * 60 * 1000));
-    let hoursms = ms % (60 * 60 * 1000);
-    let minutes = Math.floor((hoursms) / (60 * 1000));
-    let minutesms = ms % (60 * 1000);
-    let sec = Math.floor((minutesms) / (1000));
-    return days + " Hari " + hours + " Jam " + minutes + " Menit " + sec + " Detik";
-  }
+    }
