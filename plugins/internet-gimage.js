1324cecb12bbd6505b3f3e561bb3d2186d1e119f
Nayla Hanifah <138402457+Nayla-Hanifah@users.noreply.github.com>
2023-08-29 02:00:47 +0700

    Fix image

--- plugins/internet-gimage.js
+++ plugins/internet-gimage.js
@@ -1,27 +1,22 @@
-let { promisify } = require('util')
-let _gis = require('g-i-s')
-let gis = promisify(_gis)
-
-let handler  = async (m, { conn, args, text }) => {
-  if (!text) return m.reply('Cari apa?')
-  let results = await gis(text) || []
-  let { url, width, height } = pickRandom(results) || {}
-  if (!url) return m.reply('Not Found')
-  var capt = `
-*── 「 GOOGLE IMAGE 」 ──*
-
-${text}
-➸ *width*: ${width}
-➸ *height*: ${height}
+const { googleImage }  =require('@bochilteam/scraper')
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
+    const res = await googleImage(text)
+    let image = pickRandom(res)
+    let link = image
+    let caption = `*GOOGLE IMAGE*
+    
+*Result:* ${text}
+*Source:* Google
 `
-conn.sendFile(m.chat, url, 'gimage.jpg', capt, m)
+ conn.sendFile(m.chat, link, null, caption, m)
 }
-handler.help = ['image <query>', 'gimage <query>', 'googleimage <query>']
-handler.tags = ['internet']
-handler.command = /^(gimage|googleimage|image)$/i
+handler.help = ['gimage <query>', 'image <query>']
+handler.tags = ['internet', 'tools']
+handler.command = /^(gimage|image)$/i
 
 module.exports = handler
 
-function pickRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)]
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
 }
