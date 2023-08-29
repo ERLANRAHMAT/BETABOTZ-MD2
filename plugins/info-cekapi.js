9bb7babbb3734d2b894fbf3e730a93bc45deee72
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-08-30 05:26:33 +0700

    fix cekapi

--- plugins/info-cekapi.js
+++ plugins/info-cekapi.js
@@ -1,10 +1,11 @@
 const fetch = require('node-fetch')
+const util = require('util')
 
 let handler = async (m, { text }) => {
 if(!text) throw(`Masukan Apikeynya`)
-  let f = await fetch(API('lann', '/api/checkkey', { text: ${text }`))
+  let f = await fetch(`https://api.betabotz.org/api/checkkey?apikey=${text}`)
   let p = await f.json()
-  m.reply(p)
+  m.reply(util.format(p))
 }
 
 handler.command = ['cekapi']
@@ -15,3 +16,4 @@ handler.group = false
 handler.private = true
 
 module.exports = handler;
+  
