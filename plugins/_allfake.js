8261870cd651ef1f54cc7ea1fac285d62456fbbe
Nayla Hanifah <138402457+Nayla-Hanifah@users.noreply.github.com>
2023-09-06 00:59:09 +0700

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -10,7 +10,7 @@ handler.all = async function (m) {
 	} catch (e) {
 	} finally {
 		
-        global.bg = await (await fetch(img)).buffer()
+        global.bg = await (await fetch(thumb)).buffer()
 		global.doc = pickRandom(["application/vnd.ms-excel", "application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/msword", "application/pdf"])
 		
 		// Module 
@@ -23,13 +23,15 @@ handler.all = async function (m) {
          [owner[0], await this.getName(owner[0] + '@s.whatsapp.net'), 'ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ', 'Nothing', true],
          [owner[1], await this.getName(owner[1] + '@s.whatsapp.net'), 'ᴅᴇᴠᴇʟᴏᴩᴇʀ ʙᴏᴛ', 'Nothing', true], // Kalo mau di tambah tinggal copy 1baris ini di tempel di bawahnya trs di edit dikit!
         ]
-     
-                 //ini bagian image waifu
-                let waifu = JSON.parse(fs.readFileSync('./media/waifu.json'))
-                global.img = pickRandom(waifu)
+        
 		// ucapan ini mah
 		global.ucapan = ucapan()
 		global.wib = moment.tz('Asia/Jakarta').format("HH:mm:ss")
+
+//Image
+        let waifu = JSON.parse(fs.readFileSync('./media/waifu.json'))
+        global.img = pickRandom(waifu)
+        
 		// pesan sementara
 		global.ephemeral = '86400' // 86400 = 24jam, kalo ingin di hilangkan ganti '86400' jadi 'null' atau ''
 		
@@ -39,17 +41,17 @@ handler.all = async function (m) {
 				forwardingScore: 9999,
 				isForwarded: true, // ini biar ada tulisannya diteruskan berkali-kali, jika ingin di hilangkan ganti true menjadi false
 				externalAdReply: { // Bagian ini sesuka kalian berkreasi :'v
-					title: 'Time: ' + global.wib,
+					title: global.ucapan,
 					body: wm,
 					previewType: "PHOTO",
 					thumbnail: await (await fetch(img)).buffer(),
-					sourceUrl: 'https://api.betabotz.org',
+					sourceUrl: instagram,
 				}
 			}
 		}
 		
 		// Fake 🤥
-		global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: 9999999999999999999999999999999999999999999999999999999, status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
+		global.ftroli = { key: { remoteJid: 'status@broadcast', participant: '0@s.whatsapp.net' }, message: { orderMessage: { itemCount: '9999999999999999999999999999999999999999999999999999999', status: 1, surface: 1, message: wm, orderTitle: wm, sellerJid: '0@s.whatsapp.net' } } }
 		global.fkontak = { key: {participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': require('fs').readFileSync('./media/thumbnail.jpg'), thumbnail: require('fs').readFileSync('./media/thumbnail.jpg'),sendEphemeral: true}}}
 		
 	}
@@ -77,4 +79,4 @@ function ucapan() {
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
-														     }
+		}
