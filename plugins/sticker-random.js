74a9967e077170e8463bb560d9d6c957d7778727
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-12-07 19:48:59 +0700

    Update sticker-random.js

--- plugins/sticker-random.js
+++ plugins/sticker-random.js
@@ -1,42 +1,36 @@
 var { 
-sticker5 
+  sticker5 
 } = require('../lib/sticker')
 var fs = require('fs')
-var handler = async (m, {
- conn, 
- command
- }) => {
-    var error = fs.readFileSync(`./media/sticker/emror.webp`)
-    try {
-        if (command == 'dinokuning' || command == 'sdino') {
-        const res = `https://api.betabotz.org/api/sticker/dinokuning?apikey=${lann}`
-            var stiker = await sticker5(res, { packname })
-            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
-        }
-        else if (command == 'patrick' || command == 'spatrick') {
-        const res = `https://api.betabotz.org/api/sticker/patrick?apikey=${lann}`
-            var stiker = await sticker5(res, { packname })
-            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
-        }
-        else if (command == 'spongebob' || command == 'sspongebob') {
-        const res = `https://api.betabotz.org/api/sticker/spongebob?apikey=${lann}`
-            var stiker = await sticker5(res, { packname })
-            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
-        }
-        else if (command == 'doge' || command == 'sdoge') {
-        const res = `https://api.betabotz.org/api/sticker/doge?apikey=${lann}`
-            var stiker = await sticker5(res, { packname })
-            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
-        }
-        else if (command == 'manusialidi' || command == 'smanusialidi') {
-        const res = `https://api.betabotz.org/api/sticker/manusialidi?apikey=${lann}`
-            var stiker = await sticker5(res, { packname })
-            await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
-        }
-    } catch (e) {
-        console.log(e)
-        await conn.sendFile(m.chat, error, 'error.webp', '', m)
+
+var handler = async (m, { conn, command }) => {
+  var error = fs.readFileSync(`./media/sticker/emror.webp`)
+  try {
+    if (command == 'dinokuning' || command == 'sdino') {
+      const res = API('lann', '/api/sticker/dinokuning', { apikey: lann });
+      var stiker = await sticker5(res, { packname })
+      await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+    } else if (command == 'patrick' || command == 'spatrick') {
+      const res = API('lann', '/api/sticker/patrick', { apikey: lann });
+      var stiker = await sticker5(res, { packname })
+      await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+    } else if (command == 'spongebob' || command == 'sspongebob') {
+      const res = API('lann', '/api/sticker/spongebob', { apikey: lann });
+      var stiker = await sticker5(res, { packname })
+      await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+    } else if ((command == 'doge' || command == 'sdoge') {
+      const res = API('lann', '/api/sticker/doge', { apikey: lann });
+      var stiker = await sticker5(res, { packname })
+      await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
+    } else if (command == 'manusialidi' || command == 'smanusialidi') {
+      const res = API('lann', '/api/sticker/manusialidi', { apikey: lann });
+      var stiker = await sticker5(res, { packname })
+      await conn.sendFile(m.chat, stiker, 'emror.webp', '', m)
     }
+  } catch (e) {
+    console.log(e)
+    await conn.sendFile(m.chat, error, 'error.webp', '', m)
+  }
 }
 
 handler.command = handler.help = ['dinokuning', 'patrick', 'spongebob', 'doge', 'manusialidi', 'sdino', 'spatrick', 'sspongebob', 'sdoge', 'smanusialidi']
