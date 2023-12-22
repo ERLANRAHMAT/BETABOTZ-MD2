93f5b4e9a7e24074bdd16281469cd9b3b9397209
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-12-22 11:21:38 +0700

    fix ytmp3

--- plugins/downloader-ytall.js
+++ plugins/downloader-ytall.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const ytdl = require("ytdl-core");
+const ffmpeg = require("fluent-ffmpeg");
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     conn["youtubedl"] = conn["youtubedl"] || {};
@@ -32,9 +33,46 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
             const { user } = author;
 
             return new Promise(async (resolve, reject) => {
-                ytdl(args[0], {
-                    quality: "lowest",
-                }).pipe(writer);
+                if (command === 'ytmp4' || command === 'ytv') {
+                    ytdl(args[0], {
+                        quality: "lowest",
+                    }).pipe(writer);
+                } else if (command === 'ytmp3' || command === 'yta') {
+                    let audio = ytdl(args[0], { quality: 'highestaudio' });
+                    const inputFilePath = `${_filename}.webm`;
+                    const outputFilePath = `${_filename}.mp3`;
+
+                    audio.pipe(fs.createWriteStream(inputFilePath)).on('finish', async () => {
+                        ffmpeg(inputFilePath)
+                            .toFormat('mp3')
+                            .on('end', async () => {
+                                try {
+                                    let buffer = fs.readFileSync(outputFilePath);
+                                    await conn.sendMessage(m.chat, { audio: buffer, mimetype: 'audio/mpeg' }, { quoted: m });
+                                } catch (error) {
+                                    m.reply("Failed to send the audio");
+                                } finally {
+                                    fs.unlinkSync(inputFilePath);
+                                    fs.unlinkSync(outputFilePath);
+                                    delete conn["youtubedl"][m.sender];
+                                    resolve();
+                                }
+                            })
+                            .on('error', (err) => {
+                                console.log(err);
+                                m.reply(`*Convert Error:* ${err.message}`);
+                                fs.unlinkSync(inputFilePath);
+                                fs.unlinkSync(outputFilePath);
+                                delete conn["youtubedl"][m.sender];
+                                resolve();
+                            })
+                            .save(outputFilePath);
+                    });
+                } else if (command === 'ytshorts' || command === 'shorts') {
+                    ytdl(args[0], {
+                        quality: "lowest",
+                    }).pipe(writer);
+                }
 
                 writer.on("error", () => {
                     m.reply("Failed sending video");
@@ -56,9 +94,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                         } else if (command === 'ytmp3' || command === 'yta') {
                             mediaObject = {
                                 audio: {
-                                    stream: fs.createReadStream(`${_filename}.mp4`),
+                                    stream: fs.createReadStream(`${_filename}.mp3`),
                                 },
-                                caption: `┌  • *Y o u t u b e - M P 3*\n│  ◦ *Title:* ${title}\n│  ◦ *Published:* ${publishDate}\n└  ◦ *Author:* ${user}`,
+                                mimetype: 'audio/mpeg',
                             };
                         } else if (command === 'ytshorts' || command === 'shorts') {
                             mediaObject = {
@@ -75,12 +113,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
                     }
 
                     fs.unlinkSync(`${_filename}.mp4`);
+                    fs.unlinkSync(`${_filename}.mp3`);
                     delete conn["youtubedl"][m.sender];
                     resolve();
                 });
             });
         } catch {
-            m.reply("*Failed to get a video!*");
+            m.reply("*Failed to get a video or audio!*");
         }
     }
 };
@@ -91,4 +130,3 @@ handler.command = /^(ytmp4|ytv|ytmp3|yta|ytshorts|shorts)$/i;
 handler.register = false;
 
 module.exports = handler;
-              
