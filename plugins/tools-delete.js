78f96817405c4b5484ad158f1222b78228b78ed5
Eliz <138402457+Nayla-Hanifah@users.noreply.github.com>
2023-12-27 18:06:00 +0700

    Create tools-delete.js

---
+++ plugins/tools-delete.js
@@ -0,0 +1,19 @@
+let handler = async (m, { conn, command }) => {
+if (!m.quoted) throw 'Reply pesan yang ingin dihapus'
+try {
+let nay = m.message.extendedTextMessage.contextInfo.participant
+let nayla = m.message.extendedTextMessage.contextInfo.stanzaId
+return conn.sendMessage(m.chat, { delete: { remoteJid: m.chat, fromMe: false, id: nayla, participant: nay }})
+ } catch {
+return conn.sendMessage(m.chat, { delete: m.quoted.vM.key })
+}
+}
+handler.help = ['del', 'delete']
+handler.tags = ['tools']
+handler.botadmin = false
+handler.limit = true 
+handler.admin = false
+
+handler.command = /^del(ete)?$/i
+
+module.exports = handler
