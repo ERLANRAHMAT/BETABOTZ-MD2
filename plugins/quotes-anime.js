f104258fc1d59e2a5d9707a9c20dc4e25e2ad499
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-12-28 22:52:30 +0700

    Create quotes-anime.js

---
+++ plugins/quotes-anime.js
@@ -0,0 +1,28 @@
+let fetch = require('node-fetch');
+let handler = async (m, { conn }) => {
+  try {
+    let res = await fetch(API('lann', '/api/random/quotesanime', { apikey: lann }));
+    let json = await res.json();
+
+    if (json.status && json.result && json.result.length > 0) {
+      let randomIndex = Math.floor(Math.random() * json.result.length);
+      let animeQuote = json.result[randomIndex];
+
+      let cleanQuotes = animeQuote.quotes.replace(/[\n\r\t]/g, ' ');
+
+      let replyMessage = `${cleanQuotes}\n\nCharacter: ${animeQuote.karakter}\nAnime: ${animeQuote.anime}\nEpisode: ${animeQuote.episode}`;
+
+      conn.sendFile(m.chat, animeQuote.gambar, 'image.jpg', replyMessage, m, false, { contextInfo: { mentionedJid: [m.sender] } });
+    } else {
+      throw 'Invalid API response';
+    }
+  } catch (e) {
+    throw `Error: ${e.message || 'Internal server error!'}`;
+  }
+};
+
+handler.help = ['anime'];
+handler.tags = ['quotes'];
+handler.command = /^(anime)$/i;
+
+module.exports = handler;
