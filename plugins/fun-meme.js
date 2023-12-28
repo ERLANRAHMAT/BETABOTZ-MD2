10f7bc6ec68e4abcc697749cff895d273dd2bca8
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2023-12-28 22:20:13 +0700

    Update fun-meme.js

--- plugins/fun-meme.js
+++ plugins/fun-meme.js
@@ -1,9 +1,8 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, text }) => {
-let tio = await fetch('https://raw.githubusercontent.com/HasamiAini/wabot_takagisan/main/whatsapp%20bot%20takagisan/whatsapp%20bot%20takagisan/lib/memeindo.json')
-let json = await tio.json();
-let url = json[Math.floor(Math.random() * json.length)]
-await conn.sendFile(m.chat, url.image, 'file.jpg', wm, m)
+let tio = await fetch(API('lann', '/api/wallpaper/meme', { apikey: lann }))
+let json = await tio.buffer();
+await conn.sendFile(m.chat, json, 'file.jpg', wm, m)
 }
 handler.command = /^(meme)$/i
 handler.tags = ['fun']
