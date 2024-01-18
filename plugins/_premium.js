cd69c0ce55ec56b4e174652245f6f204ffc35166
Nay <138402457+Nayla-Hanifah@users.noreply.github.com>
2024-01-18 14:07:52 +0700

    Update _premium.js

--- plugins/_premium.js
+++ plugins/_premium.js
@@ -1,17 +1,11 @@
 let handler = m => m
 
 handler.before = async function (m) {
-    let user = db.data.users[m.sender]
-    if (m.chat.endsWith('status@broadcast')) {
-        console.log('Status Wa!')
-    }
-    if (user.premiumTime != 0 && user.premium) {
-        if (new Date() * 1 >= user.premiumTime) {
-            await this.reply(m.chat, `hai @${m.sender.split`@`[0]}\nwaktu premium kamu sudah habis!`, m)
+    let user = global.db.data.users[m.sender]                              
+    if (new Date() - user.premiumTime > 0) {
             user.premiumTime = 0
             user.premium = false
         }
     }
-}
 
 module.exports = handler
