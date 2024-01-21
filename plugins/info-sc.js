150175f2389a209f9d9193f97f1cef556d52db6a
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-01-21 09:03:55 +0700

    hayya

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/BOTCAHX/RTXZY-MD
+Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/ERLANRAHMAT/BETABOTZ-MD2 
 `
 m.reply(esce)
 }
