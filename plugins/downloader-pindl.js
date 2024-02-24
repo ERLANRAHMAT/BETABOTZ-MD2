6e86925e65908af7c124ef06fe013ccdb3c837cd
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-02-25 00:02:21 +0700

    Update downloader-pindl.js

--- plugins/downloader-pindl.js
+++ plugins/downloader-pindl.js
@@ -4,6 +4,9 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) {
     throw `Masukkan URL!\n\ncontoh:\n${usedPrefix}${command} https://pin.it/4CVodSq`;
   }
+  if (!args[0].startsWith('https://')) {
+    throw `Harus memasukkan URL yang valid dengan format *https://*\n\nEx: https://pin.it/4CVodSq`;
+  }
   try {
     m.reply(wait);
     const api = await fetch(`https://api.betabotz.eu.org/api/download/pinterest?url=${args[0]}&apikey=${lann}`);
