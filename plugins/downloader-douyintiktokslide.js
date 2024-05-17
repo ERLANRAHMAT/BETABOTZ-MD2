b7918376db3c13f47da5c95090d5ef59b1d8ae67
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-05-17 23:06:29 +0700

    Update and rename downloader-tiktokslide.js to downloader-douyintiktokslide.js

---
+++ plugins/downloader-douyintiktokslide.js
@@ -0,0 +1,51 @@
+const fetch = require('node-fetch');
+
+let handler = async (m, {
+  conn,
+  text,
+  usedPrefix,
+  command
+}) => {
+  if (command == 'tiktokslide' || command == 'ttslide') { // Fixed the condition for 'tiktokslide' and 'ttslide' commands
+    if (!text) throw `Masukkan URL!\n\ncontoh: ${usedPrefix + command} https://vt.tiktok.com/ZSY8Me4jL/`;
+    try {
+      const api = await fetch(`https://api.betabotz.eu.org/api/download/ttslide?url=${text}&apikey=${lann}`);
+      const res = await api.json();
+      for (let i of res.result.images) {
+        await sleep(3000);
+        conn.sendMessage(m.chat,{ image :{ url : i } , caption : `*Title*: ${res.result.title}` }, { quoted: m });
+        await sleep(5000);
+        conn.sendMessage(m.chat, { audio: { url: res.result.audio[0] }, mimetype: 'audio/mpeg' }, { quoted: m });         
+      }
+    } catch (e) {
+      console.log(e);
+      throw `🚩 *Terjadi kesalahan!*`;
+    }
+  }
+  if (command == 'douyinslide' || command == 'douyinfoto') { // Fixed the condition for 'douyinslide' and 'douyinfoto' commands
+    if (!text) throw `Masukkan URL!\n\ncontoh: ${usedPrefix + command} https://v.douyin.com/i2bPkLLo/`;
+    try {
+      const api = await fetch(`https://api.betabotz.eu.org/api/download/douyin-slide?url=${text}&apikey=${lann}`);
+      const res = await api.json();
+      for (let i of res.result.images) {
+        await sleep(3000);
+        conn.sendMessage(m.chat,{ image :{ url : i } , caption : `*Title*: ${res.result.title}` }, { quoted: m });
+        await sleep(5000);
+        conn.sendMessage(m.chat, { audio: { url: res.result.audio[0] }, mimetype: 'audio/mpeg' }, { quoted: m });         
+      }
+    } catch (e) {
+      console.log(e);
+      throw `🚩 *Terjadi kesalahan!*`;
+    }
+  }
+};
+
+handler.command = handler.help = ['douyinslide', 'douyinfoto','ttslide','tiktokslide'];
+handler.tags = ['downloader'];
+handler.limit = true;
+
+module.exports = handler;
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
