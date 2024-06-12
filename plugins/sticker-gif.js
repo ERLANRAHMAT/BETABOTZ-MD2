0f5b32a30a5ec108779ff5ac05bace173d5e1274
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-06-12 19:26:15 +0700

    Fix Sticker Toimg && Tovid

--- plugins/sticker-gif.js
+++ plugins/sticker-gif.js
@@ -1,3 +1,34 @@
+let { webp2mp4, webp2png } = require('../lib/webp2mp4');
+let { getBuffer } = require('../lib/myfunc');
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+  if (!m.quoted) throw `Balas stiker dengan caption *${usedPrefix + command}*`;
+  let mime = m.quoted.mimetype || '';
+  if (!/webp/.test(mime)) throw `Balas stiker dengan caption *${usedPrefix + command}*`;
+  let media = await m.quoted.download();
+  let out = Buffer.alloc(0);
+  m.reply(wait)
+  	
+  if (command === 'tovideo' || command === 'tovid' || command === 'togif') {
+    out = await webp2mp4(media);
+    const vd = await getBuffer(out);
+    await conn.sendFile(m.chat, vd, 'out.mp4', null, m);
+  } else if (command === 'toimg') {
+    out = await webp2png(media);
+    await conn.sendFile(m.chat, out, 'out.png', '*DONE*', m, false, {
+      thumbnail: Buffer.alloc(0)
+    });
+  }
+}
+
+handler.command = ['tovideo', 'togif', 'tovid', 'toimg'];
+handler.help = ['tovideo', 'togif', 'tovid', 'toimg (reply)'];
+handler.tags = ['sticker'];
+handler.group = false;
+
+module.exports = handler;
+
+/*
 const fs = require('fs');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
@@ -38,3 +69,4 @@ handler.tags = ['sticker'];
 handler.limit = true;
 
 module.exports = handler
+*/
