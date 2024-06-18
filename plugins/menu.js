29cd448112d7e0a54da4ccf62ac1b6b545e33d8f
dana <111495709+DanaPutra133@users.noreply.github.com>
2024-06-18 15:01:50 +0700

    Update menu.js islamic fitur

--- plugins/menu.js
+++ plugins/menu.js
@@ -28,7 +28,7 @@ let tags = {
   'nsfw': 'MENU NSFW',
   'info': 'MENU INFO',
   'internet': 'INTERNET',
-  'islam' : 'MENU ISLAMI',
+  'islamic' : 'MENU ISLAMI',
   'kerang': 'MENU KERANG',
   'maker': 'MENU MAKER',
   'owner': 'MENU OWNER',
