6cee96ae01c14710a3631430d6f26a7e82dd61c8
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-07-18 20:12:29 +0700

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,4 +1,97 @@
-let ytdl = require('ytdl-core');
+let search = require("yt-search");
+let axios = require("axios");
+
+let handler = async (m, { conn, text, usedPrefix }) => {
+    if (!text) throw 'Enter Title / Link From YouTube!';
+    try {
+        const look = await search(text);
+        const convert = look.videos[0];
+        if (!convert) throw 'Video/Audio Tidak Ditemukan';
+        if (convert.seconds >= 3600) {
+            return conn.reply(m.chat, 'Video is longer than 1 hour!', m);
+        } else {
+            let audioUrl;
+            try {
+                audioUrl = await youtube(convert.url);
+            } catch (e) {
+                conn.reply(m.chat, 'Please wait...', m);
+                audioUrl = await youtube(convert.url);
+            }
+
+            let caption = '';
+            caption += `∘ Title : ${convert.title}\n`;
+            caption += `∘ Ext : Search\n`;
+            caption += `∘ ID : ${convert.videoId}\n`;
+            caption += `∘ Duration : ${convert.timestamp}\n`;
+            caption += `∘ Viewers : ${convert.views}\n`;
+            caption += `∘ Upload At : ${convert.ago}\n`;
+            caption += `∘ Author : ${convert.author.name}\n`;
+            caption += `∘ Channel : ${convert.author.url}\n`;
+            caption += `∘ Url : ${convert.url}\n`;
+            caption += `∘ Description : ${convert.description}\n`;
+            caption += `∘ Thumbnail : ${convert.image}`;
+
+            await conn.relayMessage(m.chat, {
+                extendedTextMessage: {
+                    text: caption,
+                    contextInfo: {
+                        externalAdReply: {
+                            title: convert.title,
+                            mediaType: 1,
+                            previewType: 0,
+                            renderLargerThumbnail: true,
+                            thumbnailUrl: convert.image,
+                            sourceUrl: audioUrl.mp3
+                        }
+                    },
+                    mentions: [m.sender]
+                }
+            }, {});
+
+            await conn.sendMessage(m.chat, {
+                audio: {
+                    url: audioUrl.result.mp3
+                },
+                mimetype: 'audio/mpeg',
+                contextInfo: {
+                    externalAdReply: {
+                        title: convert.title,
+                        body: "",
+                        thumbnailUrl: convert.image,
+                        sourceUrl: audioUrl.mp3,
+                        mediaType: 1,
+                        showAdAttribution: true,
+                        renderLargerThumbnail: true
+                    }
+                }
+            }, {
+                quoted: m
+            });
+        }
+    } catch (e) {
+        conn.reply(m.chat, `*Error:* ` + e.message, m);
+    }
+};
+
+handler.command = handler.help = ['play', 'ds', 'song'];
+handler.tags = ['downloader'];
+handler.exp = 0;
+handler.limit = true;
+handler.premium = false;
+
+module.exports = handler;
+
+async function youtube(url) {
+   try {
+   const { data } = await axios.get("https://api.betabotz.eu.org/api/download/yt?url="+url+"&apikey="+lann)
+   return data;
+   } catch (e) {
+   return e;
+   }
+}
+
+//Jika mau pake module ytdl pake ini hilangin tag  /* */
+/*let ytdl = require('ytdl-core');
 let fs = require('fs');
 let ffmpeg = require('fluent-ffmpeg');
 let search = require ('yt-search');
@@ -96,3 +189,4 @@ function formatViews(views) {
     return views.toString();
   }
 }
+*/
