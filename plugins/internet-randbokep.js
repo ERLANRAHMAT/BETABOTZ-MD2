f1be0042c66d4c918b474688208d53de2b43f400
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-07-18 21:16:57 +0700

    Create internet-randbokep.js

---
+++ plugins/internet-randbokep.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+let handler = async (m, { conn }) => {
+  const q = [
+    //tambahin sendiri saya ga pernah nontooon
+    'japanese',
+    'boobs'
+  ];
+  const pick = q[Math.floor(Math.random() * q.length)];
+  try {
+    const res = await fetch(`https://api.betabotz.eu.org/api/search/xnxx?query=${pick}&apikey=${lann}`);
+    const api = await res.json();
+    
+    const ranData = await getRandomResult(api);
+
+    let capt = 乂 *R A N D O M B O K E P*\n\n;
+    capt += `  ◦ Title : ${ranData.title}\n`;
+    capt += `  ◦ Views : ${ranData.views}\n`;
+    capt += `  ◦ Quality : ${ranData.quality}\n`;
+    capt += `  ◦ Duration : ${ranData.duration}\n`;
+    capt += `  ◦ Link : ${ranData.link}\n`;
+
+    const getDl = await fetch(`https://api.betabotz.eu.org/api/download/xnxxdl?url=${ranData.link}&apikey=${lann}`)
+    conn.sendFile(m.chat, getDl.result.url, null, capt, m);
+  } catch (error) {
+    throw 🚩 *Data Tidak Ditemukan*
+  }
+}
+handler.help = handler.command = ['randombokep'];
+handler.tags = ['internet'];
+handler.premium = true;
+handler.limit = 500;
+
+module.exports = handler;
+
+function getRandomResult(data) {
+  const results = data.result;
+  const randomIndex = Math.floor(Math.random() * results.length);
+  return results[randomIndex];
+}
