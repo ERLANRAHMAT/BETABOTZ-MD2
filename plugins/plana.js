047443441e59c5f35c3c5f878dbc50a0441ba523
danaputraPC1 <danaputra1001@gmail.com>
2024-07-24 17:30:40 +0700

    plana update api

--- plugins/plana.js
+++ plugins/plana.js
@@ -1,5 +1,4 @@
-const { G4F } = require("g4f");
-let Airi = new G4F();
+const axios = require('axios');
 
 let handler = async (m, { conn, text }) => {
     conn.plana = conn.plana ? conn.plana : {};
@@ -37,27 +36,28 @@ handler.before = async (m, { conn }) => {
 
     if (conn.plana[m.sender] && m.text) {
         let name = conn.getName(m.sender);
-        const messages = [
+        const message = [
             ...conn.plana[m.sender].pesan,
             `p`,
             m.text
         ];
         try {
-      const options = [
-    {model: "gpt-4"}
-  ];
-  // di sini saya pakai model karater plana, kalau kalian mau ganti tapi model asisten bisa ganti kata plana itu dengan
-  // nama karakter yang kalian mau
-  const messages = [
+            // array buat nampung pickrandom img hapus aja // nya
+//   const img = [
+//     ``,
+//     ``,
+//     ``,
+//   ]
+  const message = [
     { role: "system", content: "kamu adalah plana, Seorang murid dari sensei di Blue archive yang siap membantu sensei kapan pun! 🍄✨" },
     { role: "assistant", content: `Kamu adalah plana, murid dari sensei blue archive, jangan gunakan saya, aku, gua gunakan plana untuk memanggil dirimu, ekspresi kan sikap kamu mirip seroang murid dari blue archive yang ada di Shittim Chest yang bernama plana, jangan gunakan GPT 4 panggil dirimu dengan plana, Kamu berbicara dengan${m.name}panggil dengan sensei, saat berbicara dengannya gunakan emoji yang lucu, Pembuatmu dana nomer WhatsApp nya ini 6281289694906 `},
     { role: "user", content: m.text },
   ];
   // ini untuk adreplay 
-  let res = await Airi.chatCompletion(messages, options);
+  let res = await aiBeta(message);
   await  conn.sendMessage(m.chat, {
     // ini nama dari karakter utama
-    text: "⬣───「 *PLANA* 」───⬣" + "\n\n" + res,
+    text: "⬣───「 *PLANA* 」───⬣" + "\n\n" + res.result,
     contextInfo: {
       externalAdReply: {  
         // title di bagian gambar
@@ -65,13 +65,14 @@ handler.before = async (m, { conn }) => {
         body: '',
         // gambar karakter kalian
         thumbnailUrl:`https://btch.pages.dev/file/abbff76a98455a64d3a07.jpg`,
+        // `${pickRandom(global.img)}`
         sourceUrl: null,
         mediaType: 1,
         renderLargerThumbnail: true
       }
     }
   }, { quoted: m });
-                conn.plana[m.sender].pesan = messages;
+                conn.plana[m.sender].pesan = message;
         } catch (e) {
             console.error("Kesalahan Dalam mengambil Data");
             throw "error";
@@ -85,11 +86,27 @@ handler.command = /^(plana)$/i
 handler.help = ["plana"];
 handler.tags = ["ai"];
 handler.limit = true;
-handler.owner = true;
-handler.group =- true
+handler.owner = false;
+handler.group = true
 
 module.exports = handler;
 
+async function aiBeta(message) {
+    return new Promise(async (resolve,reject) => { 
+        try {
+            const params = {
+                message: message,
+                apikey: `${lann}` //Ganti pake apikeymu
+            };
+            const { data } = await axios.post('https://api.betabotz.eu.org/api/search/openai-custom', params);
+            resolve(data);
+        } catch (error) {
+            reject(error);
+        };
+    });
+};
 
-// jika tidak work kalian bisa install g4f dahulu dengan $ npm i g4f 
-// atau cek dokumentasi https://www.npmjs.com/package/g4f
+//jangan lupa kalau mau pick random ini di aktifin
+// function pickRandom(list) {
+//     return list[Math.floor(list.length * Math.random())]
+//   }
