5c6c1654365376ea05596a368b6e54a5f0dddff5
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-07-24 18:59:02 +0700

    Update ai agar bisa save pertanyaan sebelumnya

--- plugins/_openai-ai.js
+++ plugins/_openai-ai.js
@@ -1,20 +1,64 @@
-var fetch = require('node-fetch');
-var handler = async (m, {
- text, 
- usedPrefix, 
- command
- }) => {
-if (!text) throw `Masukkan pertanyaan!\n\n*Contoh:* Siapa presiden Indonesia? `
-try {
-  await m.reply(wait)
-  var apii = await fetch(`https://api.betabotz.eu.org/api/search/openai-chat?text=${text}&apikey=${lann}`)
-  var res = await apii.json()
-  await m.reply(res.message)
-} catch (err) {
-  console.error(err)
-  throw "Terjadi kesalahan dalam menjawab pertanyaan"
-}
-}
+const axios = require('axios');
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (!text) throw `*Example:* ${usedPrefix + command} hai`;
+    conn.beta = conn.beta ? conn.beta : {};
+    if (!conn.beta[m.sender]) {
+        conn.beta[m.sender] = {
+            pesan: []
+        };
+        conn.beta[m.sender].timeout = setTimeout(() => {
+            delete conn.beta[m.sender];
+        }, 300000);
+
+        m.reply(`Halo \`${m.name}\`👋, Saya siap membantu anda!`);
+    } else {
+        clearTimeout(conn.beta[m.sender].timeout);
+        conn.beta[m.sender].timeout = setTimeout(() => {
+            delete conn.beta[m.sender];
+        }, 300000);
+    }
+
+    let name = conn.getName(m.sender);
+    const previousMessages = conn.beta[m.sender].pesan;
+  
+/** - Ubah prompt ini sesuaikan dengan keinginan mu 
+    - Usahakan berikan logic yang masuk akal dan mudah di pahami!
+**/
+    const messages = [
+        { role: "system", content: "Kamu adalah BetaBotz Ai Sebuah Ai Yang diciptakan oleh Lann,bantu setiap orang dengan ramah:),berikan emoticon di setiap jawaban" },
+        { role: "assistant", content: `Kamu adalah BetaBotz Ai,ai bot yang diciptakan oleh Lann untuk membantu semua permintaan dari user,jawab setiap pertanyaan dengan ramah dan sertai emoticon` },
+        ...previousMessages.map((msg, i) => ({ role: i % 2 === 0 ? 'user' : 'assistant', content: msg })),
+        { role: "user", content: text }
+    ];
+    try {
+        const aiBeta = async function(message) {
+            return new Promise(async (resolve, reject) => {
+                try {
+                    const params = {
+                        message: message,
+                        apikey: lann
+                    };
+                    const { data } = await axios.post('https://api.betabotz.eu.org/api/search/openai-custom', params);
+                    resolve(data);
+                } catch (error) {
+                    reject(error);
+                }
+            });
+        };
+
+        let res = await aiBeta(messages);
+        if (res && res.result) {
+            await m.reply(res.result);
+            conn.beta[m.sender].pesan = messages.map(msg => msg.content);
+        } else {
+            throw "Kesalahan dalam mengambil data";
+        }
+    } catch (e) {
+        throw eror
+    }
+};
+
 handler.command = handler.help = ['ai','openai','chatgpt'];
 handler.tags = ['tools'];
 handler.premium = false
