92df03227aa1ef9b12d030f61a8094c26f52d80f
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2024-09-29 09:58:28 +0700

    Update tools-ogg.js

--- plugins/tools-ogg.js
+++ plugins/tools-ogg.js
@@ -1,27 +1,24 @@
-const { toAudio, toPTT } = require('../lib/converter')
+const fetch = require('node-fetch');
+const uploader = require('../lib/uploadFile');
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-  let q = m.quoted ? m.quoted : m
-  let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (/mp3|a(udio)?$/i.test(command)) {
-    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
-    let media = await q.download()
-    if (!media) throw 'Media tidak dapat diunduh'
-    let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw 'Gagal melakukan konversi.'
-    conn.sendMessage(m.chat, {document: audio.data, mimetype: 'audio/mpeg', fileName: `audio.mp3`}, { quoted : m })
-  }
-  if (/vn|ptt$/i.test(command)) {
-    if (!/video|audio/.test(mime)) throw `Balas video/audio dengan perintah *${usedPrefix + command}*`
-    let media = await q.download()
-    if (!media) throw 'Media tidak dapat diunduh'
-    let audio = await toPTT(media, 'mp4')
-    if (!audio.data) throw 'Gagal melakukan konversi.'
-    await conn.sendFile(m.chat, audio.data, 'file.mp4', '', m, 1, { mimetype: 'audio/mp4' })
-  }
+	let q = m.quoted ? m.quoted : m
+	let mime = (q.msg || q).mimetype || q.mediaType || ''
+	if (/video/.test(mime)) {
+		let buffer = await q.download()
+		await m.reply(wait)
+		try {
+		let media = await uploader(buffer)
+		let json = await (await fetch(`https://api.betabotz.eu.org/api/tools/video2audio?url=${media}&apikey=${lann}`)).json()		
+        await conn.sendFile(m.chat, json.result, "audio.mp3", "*DONE*", m)
+        } catch (err) {
+      throw eror
+    }
+ } else throw `Reply video with command ${usedPrefix + command}`
 }
-handler.help = ['tomp3', 'tovn']
-handler.tags = ['Pengubah Suara']
-handler.command = /^to(mp3|vn|ptt)$/i
+handler.help = ['video2audio']
+handler.tags = ['tools']
+handler.command = /^(video2audio)$/i
+handler.limit = true;
 
 module.exports = handler
