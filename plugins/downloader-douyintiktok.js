d5bbb6234639221e3ada3c016eec4927d7671b07
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2025-03-13 06:19:41 +0700

    Update downloader-douyintiktok.js

--- plugins/downloader-douyintiktok.js
+++ plugins/downloader-douyintiktok.js
@@ -11,10 +11,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         throw `URL Tidak Ditemukan!`;
     }
 
-    if (command === 'tiktok' || command === 'tt' || command === 'tiktokdl' || command === 'tiktoknowm') {
+    if (command === 'tiktok' || command === 'tt' || command === 'ttdl' || command === 'ttnowm' || command === 'tiktokdl' || command === 'tiktoknowm') {
         capt = `乂 *T I K T O K*`;
         urlApi = `https://api.betabotz.eu.org/api/download/tiktok?url=${text}&apikey=${lann}`;
-    } else if (command === 'douyin') {
+    } else if (command === 'douyin' || command = 'douyindl') {
         capt = `乂 *D O U Y I N*`;
         urlApi = `https://api.betabotz.eu.org/api/download/douyin?url=${text}&apikey=${lann}`;
     }
@@ -42,8 +42,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
         throw `🚩 Terjadi kesalahan, coba lagi nanti.`;
     }
 };
-
-handler.help = handler.command = /^(tiktok|tt|tiktokdl|tiktoknowm|douyin)$/i;
+handler.help = handler.command = ['tiktok', 'tt', 'ttdl', 'ttnowm', 'tiktokdl', 'tiktoknowm', 'douyin', 'douyindl'];
 handler.tags = ['downloader'];
 handler.limit = true;
 handler.group = false;
