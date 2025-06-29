aa72ba139137d06a60affb770b4eb28ab2d4e965
Lann <98427364+ERLANRAHMAT@users.noreply.github.com>
2025-06-29 20:36:00 +0700

    Grow and garden fitur

---
+++ plugins/growgarden.js
@@ -0,0 +1,46 @@
+let fetch = require('node-fetch');
+
+let handler = async (m, { usedPrefix, command }) => {
+    m.reply(wait);
+    try {
+        let res = await (await fetch(`https://api.betabotz.eu.org/api/webzone/grow-and-garden-stock?apikey=${lann}`)).json();
+        let content = `*🌱 G R O W  &  G A R D E N  S T O C K S 🌱*\n\n`;
+
+        if (res.status && res.result && res.result.data) {
+            let gearStocks = res.result.data.filter(item => item.category === "GEAR STOCKS");
+            let eggStocks = res.result.data.filter(item => item.category === "EGG STOCKS");
+            let seedsStocks = res.result.data.filter(item => item.category === "SEEDS STOCKS");
+            let eventStocks = res.result.data.filter(item => item.category === "EVENT STOCKS");
+
+            content += `*🛠️ Gear Stocks:*\n`;
+            gearStocks.forEach(item => {
+                content += `  ◦ ${item.name}: ${item.count}\n`;
+            });
+
+            content += `\n*🥚 Egg Stocks:*\n`;
+            eggStocks.forEach(item => {
+                content += `  ◦ ${item.name}: ${item.count}\n`;
+            });
+
+            content += `\n*🌾 Seeds Stocks:*\n`;
+            seedsStocks.forEach(item => {
+                content += `  ◦ ${item.name}: ${item.count}\n`;
+            });
+
+            content += `\n*🎉 Event Stocks:*\n`;
+            eventStocks.forEach(item => {
+                content += `  ◦ ${item.name}: ${item.count}\n`;
+            });
+        } else {
+            content += 'Data stok tidak ditemukan.';
+        }
+        await m.reply(content);
+    } catch (error) {
+        throw eror
+    }
+};
+
+handler.command = handler.help = ['growgarden'];
+handler.tags = ['internet'];
+handler.limit = true;
+module.exports = handler;
