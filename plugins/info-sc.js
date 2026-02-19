5c22bc77618cf848f5eb8d2afb3029c76d8501a6
danaputra133 <danaputra375@gmail.com>
2026-02-19 22:33:48 +0700

    update info dc

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,12 +1,13 @@
 let handler = async (m, { conn }) => {
-let ye = `@${m.sender.split`@`[0]}`
+let ye = `@${m.sender.split`@`[0]}`;
 let esce = `
-Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/ERLANRAHMAT/BETABOTZ-MD2 
-`
-m.reply(esce)
-}
-handler.help = ['sc', 'sourcecode']
-handler.tags = ['info']
-handler.command = /^(sc|sourcecode)$/i
+Hai ${ye} Bot Ini Menggunakan Script :\n• https://github.com/ERLANRAHMAT/BETABOTZ-MD2 \ndan customasi dari\nhttps://github.com/DanaPutra133/AQUABOT-V4
+\n jangan lupa di follow dan Bintang nya! :)
+`;
+  m.reply(esce);
+};
+handler.help = ["sc", "sourcecode"];
+handler.tags = ["info"];
+handler.command = /^(sc|sourcecode)$/i;
 
-module.exports = handler
+module.exports = handler;
