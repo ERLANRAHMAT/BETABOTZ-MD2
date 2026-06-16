edf8c0beaf274f579bbc000bc74f7c500d35737c
danaputra133 <danaputra375@gmail.com>
2026-06-17 06:52:30 +0700

    antitagsw testing

--- plugins/_antitagsw.js
+++ plugins/_antitagsw.js
@@ -1,66 +1,68 @@
-    let handler = m => m
+let handler = m => m
 
-    handler.before = async (m, { conn, isBotAdmin, isAdmin }) => {
-        if(!m.isGroup) return
-        let chat = global.db.data.chats[m.chat]
-        if (chat.antitagsw) {
-        const isTaggingInStatus = (
-            m.mtype === 'groupStatusMention' ||
-            m.mtype === 'groupStatusMentionMessage' ||
-            m.message?.groupStatusMention ||
-            m.message?.groupStatusMentionMessage
-        )
-        
-        if (!isTaggingInStatus) return
-        
-        await conn.sendMessage(m.chat, {delete: {remoteJid: m.chat,fromMe: false, id: m.key.id, participant: m.sender, }, });
-        }
-    }
-
-    module.exports = handler
-
-    // kode di bawah jika mau user nya di kick.
-    /*
-
-
-    let handler = m => m
-
-    handler.before = async (m, { conn, isBotAdmin, isAdmin }) => {
-        if(!m.isGroup) return
-        let chat = global.db.data.chats[m.chat]
-        if (chat.antitagsw) {
-        const isTaggingInStatus = (
-            m.mtype === 'groupStatusMentionMessage' || 
-            (m.quoted && m.quoted.mtype === 'groupStatusMentionMessage') ||
-            (m.message && m.message.groupStatusMentionMessage) ||
-            (m.quoted && m.quoted.mtype === 'groupStatusMention') ||
-            (m.message && m.message.groupStatusMention) ||
-            (m.message && m.message.protocolMessage && m.message.protocolMessage.type === 25)
-        )
-        
-        if (!isTaggingInStatus) return
-        
-        await conn.sendMessage(m.chat, { delete: m.key })
+handler.before = async (m, { conn, isBotAdmin, isAdmin }) => {
+    if(!m.isGroup) return
+    let chat = global.db.data.chats[m.chat]
+    if (chat.antitagsw) {
+    const isTaggingInStatus = (
+        m.mtype === 'groupStatusMentionMessage' || 
+        (m.quoted && m.quoted.mtype === 'groupStatusMentionMessage') ||
+        (m.quoted && m.quoted.mtype === 'groupStatusMention') ||
+        (m.message && m.message.groupStatusMention) ||
+        (m.message && m.message.groupStatusMentionMessage) ||
+        (m.message && m.message.protocolMessage && m.message.protocolMessage.type === 25)
+    )
     
-        if (isAdmin) { // nambahin jika admin maka ha di kick cuma hapus pesan aja
-            let warningMessage = `Grup ini terdeteksi ditandai dalam Status WhatsApp\n\n` +
-                                `@${m.sender.split("@")[0]}, mohon untuk tidak menandai grup dalam status WhatsApp` +
-                                `\n\nHal tersebut tidak diperbolehkan dalam grup ini.`
-            
-            return conn.sendMessage(m.chat, { text: warningMessage, mentions: [m.sender] })
-        }
-        
-        if (isBotAdmin) {
-            await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
-            await conn.sendMessage(m.chat, { text: `@${m.sender.split("@")[0]} telah dikeluarkan dari grup karena menandai grup dalam status WhatsApp.`, mentions: [m.sender] })
-        } else {
-            let warningMessage = `Grup ini terdeteksi ditandai dalam Status WhatsApp\n\n` +
-                                `@${m.sender.split("@")[0]}, mohon untuk tidak menandai grup dalam status WhatsApp` +
-                                `\n\nHal tersebut tidak diperbolehkan dalam grup ini.`
-            
-            return conn.sendMessage(m.chat, { text: warningMessage, mentions: [m.sender] })
-        }
+    if (!isTaggingInStatus) return
+    
+    await conn.sendMessage(m.chat, { delete: m.key })
     }
-    }
+}
 
-    module.exports = handler */
+module.exports = handler
+
+// kode di bawah jika mau user nya di kick.
+/*
+
+
+let handler = m => m
+
+handler.before = async (m, { conn, isBotAdmin, isAdmin }) => {
+    if(!m.isGroup) return
+    let chat = global.db.data.chats[m.chat]
+    if (chat.antitagsw) {
+    const isTaggingInStatus = (
+        m.mtype === 'groupStatusMentionMessage' || 
+        (m.quoted && m.quoted.mtype === 'groupStatusMentionMessage') ||
+        (m.message && m.message.groupStatusMentionMessage) ||
+        (m.quoted && m.quoted.mtype === 'groupStatusMention') ||
+        (m.message && m.message.groupStatusMention) ||
+        (m.message && m.message.protocolMessage && m.message.protocolMessage.type === 25)
+    )
+    
+    if (!isTaggingInStatus) return
+    
+    await conn.sendMessage(m.chat, { delete: m.key })
+   
+    if (isAdmin) { // nambahin jika admin maka ha di kick cuma hapus pesan aja
+        let warningMessage = `Grup ini terdeteksi ditandai dalam Status WhatsApp\n\n` +
+                            `@${m.sender.split("@")[0]}, mohon untuk tidak menandai grup dalam status WhatsApp` +
+                            `\n\nHal tersebut tidak diperbolehkan dalam grup ini.`
+        
+        return conn.sendMessage(m.chat, { text: warningMessage, mentions: [m.sender] })
+    }
+    
+    if (isBotAdmin) {
+        await conn.groupParticipantsUpdate(m.chat, [m.sender], "remove")
+        await conn.sendMessage(m.chat, { text: `@${m.sender.split("@")[0]} telah dikeluarkan dari grup karena menandai grup dalam status WhatsApp.`, mentions: [m.sender] })
+    } else {
+        let warningMessage = `Grup ini terdeteksi ditandai dalam Status WhatsApp\n\n` +
+                            `@${m.sender.split("@")[0]}, mohon untuk tidak menandai grup dalam status WhatsApp` +
+                            `\n\nHal tersebut tidak diperbolehkan dalam grup ini.`
+        
+        return conn.sendMessage(m.chat, { text: warningMessage, mentions: [m.sender] })
+      }
+   }
+}
+
+module.exports = handler */
