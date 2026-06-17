d85de68de3c411b300e57a58dfb3112f704637b2
danaputra133 <danaputra375@gmail.com>
2026-06-17 09:54:49 +0700

    males gak usah dah

--- plugins/group-totalpesan.js
+++ plugins/group-totalpesan.js
@@ -1,88 +1,88 @@
-let handler = async (m, { conn }) => {
-    const user = global.db.data.chats[m.chat]?.memgc || {};
+// let handler = async (m, { conn }) => {
+//     const user = global.db.data.chats[m.chat]?.memgc || {};
 
-    let memgc = Object.keys(user)
-        .filter(jid => {
-            const isUser =
-                jid.endsWith('@s.whatsapp.net') ||
-                jid.endsWith('@lid');
-            const isGroup = jid.endsWith('@g.us');
+//     let memgc = Object.keys(user)
+//         .filter(jid => {
+//             const isUser =
+//                 jid.endsWith('@s.whatsapp.net') ||
+//                 jid.endsWith('@lid');
+//             const isGroup = jid.endsWith('@g.us');
 
-            return isUser && !isGroup;
-        })
-        .sort((a, b) => {
-            const totalA = user[a]?.chat || 0;
-            const totalB = user[b]?.chat || 0;
-            return totalB - totalA;
-        });
+//             return isUser && !isGroup;
+//         })
+//         .sort((a, b) => {
+//             const totalA = user[a]?.chat || 0;
+//             const totalB = user[b]?.chat || 0;
+//             return totalB - totalA;
+//         });
 
-    if (!memgc.length) {
-        return m.reply('Belum ada data statistik chat di grup ini.');
-    }
+//     if (!memgc.length) {
+//         return m.reply('Belum ada data statistik chat di grup ini.');
+//     }
 
-    let nomor = 1;
-    let chatToday = 0;
-    let chatTotal = 0;
+//     let nomor = 1;
+//     let chatToday = 0;
+//     let chatTotal = 0;
 
-    for (const jid of memgc) {
-        chatToday += Number(user[jid]?.chat || 0);
-        chatTotal += Number(user[jid]?.chatTotal || 0);
-    }
+//     for (const jid of memgc) {
+//         chatToday += Number(user[jid]?.chat || 0);
+//         chatTotal += Number(user[jid]?.chatTotal || 0);
+//     }
 
-    let head =
-        `Total chat group hari ini: ${toRupiah(chatToday)}\n` +
-        `Total semua chat: ${toRupiah(chatTotal)}\n\n`;
+//     let head =
+//         `Total chat group hari ini: ${toRupiah(chatToday)}\n` +
+//         `Total semua chat: ${toRupiah(chatTotal)}\n\n`;
 
-    let caption = '';
+//     let caption = '';
 
-    for (let i = 0; i < Math.min(memgc.length, 20); i++) {
-        const jid = memgc[i];
-        const data = user[jid] || {};
+//     for (let i = 0; i < Math.min(memgc.length, 20); i++) {
+//         const jid = memgc[i];
+//         const data = user[jid] || {};
         
 
         
-        caption += `*${nomor++}.* ${conn.getName(jid)}\n`;
-        caption += `Chat Today : ${toRupiah(data.chat || 0)}\n`;
-        caption += `Total Chat : ${toRupiah(data.chatTotal || 0)}\n`;
-        caption += `Last Chat : ${getTime(data.lastseen)}\n\n`;
-    }
+//         caption += `*${nomor++}.* ${conn.getName(jid)}\n`;
+//         caption += `Chat Today : ${toRupiah(data.chat || 0)}\n`;
+//         caption += `Total Chat : ${toRupiah(data.chatTotal || 0)}\n`;
+//         caption += `Last Chat : ${getTime(data.lastseen)}\n\n`;
+//     }
 
-    await m.reply(head + caption.trim());
-};
+//     await m.reply(head + caption.trim());
+// };
 
-handler.help = ['totalchatgc'];
-handler.tags = ['group'];
-handler.command = /^(totalchatgc)$/i;
-handler.admin = true;
-handler.group = true;
+// handler.help = ['totalchatgc'];
+// handler.tags = ['group'];
+// handler.command = /^(totalchatgc)$/i;
+// handler.admin = true;
+// handler.group = true;
 
-module.exports = handler;
+// module.exports = handler;
 
-function parseMs(ms) {
-    return {
-        days: Math.trunc(ms / 86400000),
-        hours: Math.trunc(ms / 3600000) % 24,
-        minutes: Math.trunc(ms / 60000) % 60,
-        seconds: Math.trunc(ms / 1000) % 60
-    };
-}
+// function parseMs(ms) {
+//     return {
+//         days: Math.trunc(ms / 86400000),
+//         hours: Math.trunc(ms / 3600000) % 24,
+//         minutes: Math.trunc(ms / 60000) % 60,
+//         seconds: Math.trunc(ms / 1000) % 60
+//     };
+// }
 
-function getTime(ms) {
-    if (!ms || isNaN(ms)) return 'Belum pernah';
+// function getTime(ms) {
+//     if (!ms || isNaN(ms)) return 'Belum pernah';
 
-    const diff = Date.now() - Number(ms);
+//     const diff = Date.now() - Number(ms);
 
-    if (diff < 0) return 'Baru saja';
+//     if (diff < 0) return 'Baru saja';
 
-    const now = parseMs(diff);
+//     const now = parseMs(diff);
 
-    if (now.days) return `${now.days} hari lalu`;
-    if (now.hours) return `${now.hours} jam lalu`;
-    if (now.minutes) return `${now.minutes} menit lalu`;
+//     if (now.days) return `${now.days} hari lalu`;
+//     if (now.hours) return `${now.hours} jam lalu`;
+//     if (now.minutes) return `${now.minutes} menit lalu`;
 
-    return 'Baru saja';
-}
+//     return 'Baru saja';
+// }
 
-function toRupiah(number) {
-    return Number(number || 0).toLocaleString('id-ID');
-}
+// function toRupiah(number) {
+//     return Number(number || 0).toLocaleString('id-ID');
+// }
