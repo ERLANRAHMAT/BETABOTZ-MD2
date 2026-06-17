a8937ca5c5a2efe2c9f4f36d89ff4600c8cfb213
danaputra133 <danaputra375@gmail.com>
2026-06-17 09:42:57 +0700

    totalchat gc testing

--- plugins/group-totalpesan.js
+++ plugins/group-totalpesan.js
@@ -55,12 +55,8 @@ let handler = async (m, { conn }) => {
     let nomor = 1;
 
     for (let i = 0; i < Math.min(memgc.length, 20); i++) {
-       
         const jid = memgc[i];
         const data = user[jid] || {};
-        if (!user[jid]?.name && (p?.name || p?.notify)) {
-            user[jid].name = p?.name || p?.notify;
-        }
 
         let name = getNameFromGroup(jid);
 
