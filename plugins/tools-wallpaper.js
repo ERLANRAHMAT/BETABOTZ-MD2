7820220565242033253e1efb0043479af5ea3435
dana <danaputra375@gmail.com>
2026-07-31 10:36:55 +0700

    hehe lupa

--- plugins/tools-wallpaper.js
+++ plugins/tools-wallpaper.js
@@ -1,17 +1,20 @@
-const fetch = require('node-fetch');
+import fetch from 'node-fetch';
 
 let handler = async (m, { conn }) => {
     try {
         await m.reply(wait); 
-        let apiUrl = `https://api.betabotz.eu.org/api/wallpaper/wallhp?apikey=${global.lann}`;
+        
+        let apiUrl = `https://api.betabotz.eu.org/api/wallpaper/wallhp?apikey=${lann}`;
+        
         await conn.sendMessage(m.chat, {
             image: { url: apiUrl }, 
             caption: `Berikut adalah wallpaper random untuk Anda!`,
         }, { quoted: m });
-    }  catch (e) {
-    console.log(e);
-    throw e;
-  }
+        
+    } catch (e) {
+        console.log(e);
+        throw e;
+    }
 }
 
 handler.tags = ['image', 'internet'];
