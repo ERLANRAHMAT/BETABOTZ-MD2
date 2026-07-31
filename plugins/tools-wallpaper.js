c3621e4714d209c608f02294b0e8e495cbc11c0e
dana <danaputra375@gmail.com>
2026-07-31 10:20:29 +0700

    update main.js + fitur yang result nya berubah dari API

--- plugins/tools-wallpaper.js
+++ plugins/tools-wallpaper.js
@@ -1,16 +1,19 @@
+const fetch = require('node-fetch');
+
 let handler = async (m, { conn }) => {
     try {
-        
-        let apiUrl = `https://api.betabotz.eu.org/api/wallpaper/wallhp?apikey=${lann}`;
-
-        
+        await m.reply(wait); 
+        let apiUrl = `https://api.betabotz.eu.org/api/wallpaper/wallhp?apikey=${global.lann}`;
         await conn.sendMessage(m.chat, {
             image: { url: apiUrl }, 
             caption: `Berikut adalah wallpaper random untuk Anda!`,
         }, { quoted: m });
+        
     } catch (error) {
         console.error(error);
-        throw `Terjadi kesalahan: ${error.message || error}`;
+        if (error !== false) {
+             throw `Terjadi kesalahan: ${error.message || error}`;
+        }
     }
 };
 
