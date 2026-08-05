46ee8d64bef539a17d257f82b188a04a6b86efce
dana <danaputra375@gmail.com>
2026-08-05 10:58:48 +0700

    update V3.1.0

--- plugins/owner-getplugins.js
+++ plugins/owner-getplugins.js
@@ -6,13 +6,13 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let handler = async (m, { usedPrefix, command, text }) => {
-    if (!text) throw `where is the text?\n\nexempel: ${usedPrefix + command} menu`
+    if (!text) throw `text nya mana?\n\contoh: ${usedPrefix + command} menu`
     
     const filename = path.join(__dirname, `./${text}${!/\.js$/i.test(text) ? '.js' : ''}`)
     const listPlugins = fs.readdirSync(__dirname).map(v => v.replace(/\.js/, ''))
     
     if (!fs.existsSync(filename)) return m.reply(`
-'${filename}' not found!
+'${filename}' tidak ditemukan!
 
 *List Plugins:*
 ${listPlugins.join('\n').trim()}
