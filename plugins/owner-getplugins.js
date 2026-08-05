c851adf65469fb498f3831d93225cf0167fcd43d
dana <danaputra375@gmail.com>
2026-08-05 10:57:30 +0700

    update V3.1.0

--- plugins/owner-getplugins.js
+++ plugins/owner-getplugins.js
@@ -1,15 +1,26 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 let handler = async (m, { usedPrefix, command, text }) => {
     if (!text) throw `where is the text?\n\nexempel: ${usedPrefix + command} menu`
+    
     const filename = path.join(__dirname, `./${text}${!/\.js$/i.test(text) ? '.js' : ''}`)
-    const listPlugins = fs.readdirSync(path.join(__dirname)).map(v => v.replace(/\.js/, ''))
+    const listPlugins = fs.readdirSync(__dirname).map(v => v.replace(/\.js/, ''))
+    
     if (!fs.existsSync(filename)) return m.reply(`
 '${filename}' not found!
-${listPlugins.map(v => v).join('\n').trim()}
+
+*List Plugins:*
+${listPlugins.join('\n').trim()}
 `.trim())
+
     m.reply(fs.readFileSync(filename, 'utf8'))
 }
+
 handler.help = ['getplugin'].map(v => v + ' [filename]')
 handler.tags = ['owner']
 handler.command = /^(getplugin|get ?plugin|gp)$/i
