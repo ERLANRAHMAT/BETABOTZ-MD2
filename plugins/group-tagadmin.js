2cdf4d870f4c88214017f832c0db7da83f5b7d48
dana <danaputra375@gmail.com>
2026-08-19 21:35:46 +0700

    fix tagadmin

--- plugins/group-tagadmin.js
+++ plugins/group-tagadmin.js
@@ -1,9 +1,11 @@
 let handler = async (m, { conn, participants, groupMetadata }) => {
 
     const getGroupAdmins = (participants) => {
-        admins = []
+        let admins = [] 
         for (let i of participants) {
-            i.admin === "admin" ? admins.push(i.id) : ''
+            if (i.admin === "admin" || i.admin === "superadmin") {
+                admins.push(i.id)
+            }
         }
         return admins
     }
@@ -12,6 +14,7 @@ let handler = async (m, { conn, participants, groupMetadata }) => {
     try {
         pp = await conn.profilePictureUrl(m.chat, 'image')
     } catch (e) {
+        console.error(e)
     } finally {
         let { isBanned, welcome, detect, sWelcome, sBye, sPromote, sDemote, antiLink } = global.db.data.chats[m.chat]
         const groupAdmins = getGroupAdmins(participants)
@@ -27,7 +30,7 @@ ${groupMetadata.subject}
 *Group Admins:*
 ${listAdmin}
 `.trim()
-        ownernya = [`${m.chat.split`-`[0]}@s.whatsapp.net`]
+        let ownernya = [`${m.chat.split`-`[0]}@s.whatsapp.net`] // Ditambahkan 'let' di sini
         let mentionedJid = groupAdmins.concat(ownernya)
         conn.sendFile(m.key.remoteJid, pp, 'pp.jpg', text, m, false, { contextInfo: { mentionedJid } })
     }
