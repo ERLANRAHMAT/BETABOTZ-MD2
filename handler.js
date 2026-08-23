44663bb7518b94b4f90d045d9cd9fd2c38f229f1
dana <danaputra375@gmail.com>
2026-08-23 20:42:05 +0700

    handler fix prefix ke config test

--- handler.js
+++ handler.js
@@ -1058,27 +1058,21 @@ export default {
                 if (!plugin) continue
                 if (plugin.disabled) continue
                 if (!opts['restrict']) if (plugin.tags && plugin.tags.includes('admin')) {
-                    // global.dfail('restrict', m, this)
-                    continue
+                continue
                 }
         const str2Regex = (str) => str.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&");
-        let _prefix = plugin.customPrefix
-          ? plugin.customPrefix
-          : conn.prefix
-            ? conn.prefix
-            : global.prefix;
+
+        let _prefix = global.prefix;
+
         let match = (
-          _prefix instanceof RegExp // RegExp Mode?
+          _prefix instanceof RegExp
             ? [[_prefix.exec(m.text), _prefix]]
-            : Array.isArray(_prefix) // Array?
+            : Array.isArray(_prefix)
               ? _prefix.map((p) => {
-                  let re =
-                    p instanceof RegExp // RegExp in Array?
-                      ? p
-                      : new RegExp(str2Regex(p));
+                  let re = p instanceof RegExp ? p : new RegExp(str2Regex(p));
                   return [re.exec(m.text), re];
                 })
-              : typeof _prefix === "string" // String?
+              : typeof _prefix === "string"
                 ? [
                     [
                       new RegExp(str2Regex(_prefix)).exec(m.text),
