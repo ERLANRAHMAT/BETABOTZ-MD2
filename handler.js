4da267ae93e682e5d45b45e2602c3ac69d0801ed
dana <danaputra375@gmail.com>
2026-08-23 20:57:12 +0700

    handler fix prefix ke config test

--- handler.js
+++ handler.js
@@ -996,6 +996,26 @@ export default {
                 this.msgqueque.push(m.id || m.key.id)
                 await delay(this.msgqueque.length * 1000)
             }
+            const prefix = global.prefix;
+
+            for (let name in global.plugins) {
+              let plugin = global.plugins[name];
+
+              if (!plugin) continue;
+              if (plugin.disabled) continue;
+              if (!plugin.all) continue;
+              if (typeof plugin.all !== "function") continue;
+
+              if (typeof prefix === "string" && !m.text.startsWith(prefix))
+                continue;
+
+              try {
+                await plugin.all.call(this, m, chatUpdate);
+              } catch (e) {
+                if (typeof e === "string") continue;
+                console.error(e);
+              }
+            }
             for (let name in global.plugins) {
                 let plugin = global.plugins[name]
                 if (!plugin) continue
@@ -1062,66 +1082,57 @@ export default {
                     continue
                 }
         const str2Regex = (str) => str.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&");
-
-let _prefix = plugin.customPrefix
-  ? plugin.customPrefix
-  : global.prefix;
-
-let match = (
-  _prefix instanceof RegExp
-    ? [[_prefix.exec(m.text), _prefix]]
-    : Array.isArray(_prefix)
-      ? _prefix.map((p) => {
-          let re =
-            p instanceof RegExp
-              ? p
-              : new RegExp(str2Regex(p));
-
-          return [re.exec(m.text), re];
-        })
-      : typeof _prefix === "string"
-        ? [
-            [
-              new RegExp(str2Regex(_prefix)).exec(m.text),
-              new RegExp(str2Regex(_prefix)),
-            ],
-          ]
-        : [[null, new RegExp()]]
-).find((p) => p[0]);
-
-if (typeof plugin.before === "function")
-  if (
-    await plugin.before.call(this, m, {
-      match,
-      conn: this,
-      participants,
-      groupMetadata,
-      user,
-      bot,
-      isROwner,
-      isOwner,
-      isAdmin,
-      isBotAdmin,
-      isPrems,
-      chatUpdate,
-    })
-  )
-    continue;
-
-if (typeof plugin !== "function") continue;
-
-if ((usedPrefix = (match?.[0] || '')[0])) {
-    let noPrefix = m.text.replace(usedPrefix, '');
-
-    let [command, ...args] = noPrefix.trim().split` `.filter(v => v);
-
-    args = args || [];
-
-    let _args = noPrefix.trim().split` `.slice(1);
-
-    let text = _args.join` `;
-
-    command = (command || '').toLowerCase();
+        let _prefix = plugin.customPrefix
+          ? plugin.customPrefix
+          : conn.prefix
+            ? conn.prefix
+            : global.prefix;
+        let match = (
+          _prefix instanceof RegExp // RegExp Mode?
+            ? [[_prefix.exec(m.text), _prefix]]
+            : Array.isArray(_prefix) // Array?
+              ? _prefix.map((p) => {
+                  let re =
+                    p instanceof RegExp // RegExp in Array?
+                      ? p
+                      : new RegExp(str2Regex(p));
+                  return [re.exec(m.text), re];
+                })
+              : typeof _prefix === "string" // String?
+                ? [
+                    [
+                      new RegExp(str2Regex(_prefix)).exec(m.text),
+                      new RegExp(str2Regex(_prefix)),
+                    ],
+                  ]
+                : [[[], new RegExp()]]
+        ).find((p) => p[1]);
+        if (typeof plugin.before === "function")
+          if (
+            await plugin.before.call(this, m, {
+              match,
+              conn: this,
+              participants,
+              groupMetadata,
+              user,
+              bot,
+              isROwner,
+              isOwner,
+              isAdmin,
+              isBotAdmin,
+              isPrems,
+              chatUpdate,
+            })
+          )
+            continue;
+                if (typeof plugin !== 'function') continue
+                if ((usedPrefix = (match[0] || '')[0])) {
+                    let noPrefix = m.text.replace(usedPrefix, '')
+                    let [command, ...args] = noPrefix.trim().split` `.filter(v => v)
+                    args = args || []
+                    let _args = noPrefix.trim().split` `.slice(1)
+                    let text = _args.join` `
+                    command = (command || '').toLowerCase()
                     let fail = plugin.fail || global.dfail // When failed
                     let isAccept = plugin.command instanceof RegExp ? // RegExp Mode?
                         plugin.command.test(command) :
