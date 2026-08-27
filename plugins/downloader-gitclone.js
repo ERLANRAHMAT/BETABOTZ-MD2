b892c73f8ccd168351269d71ba3290c8e87ed13b
dana <danaputra375@gmail.com>
2026-08-27 15:05:59 +0700

    BAILEYS | UPDATE & FIX 27 ~ xx

--- plugins/downloader-gitclone.js
+++ plugins/downloader-gitclone.js
@@ -1,19 +1,40 @@
-let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
 import fetch from 'node-fetch';
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     try {
-        if (!args[0]) throw 'link githubnya mana? contoh: https://github.com/BOTCAHX/RTXZY-MD'
+        if (!args[0]) throw `Link githubnya mana?\nContoh: ${usedPrefix + command} https://github.com/BOTCAHX/RTXZY-MD`;
 
-        if (!regex.test(args[0])) throw 'link salah!'
+        let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/([^\/]+)(?:\/tree\/([^\/]+))?/i;
+        
+        if (!regex.test(args[0])) throw 'Link GitHub tidak valid!';
 
-        let [, user, repo] = args[0].match(regex) || []
-        repo = repo.replace(/.git$/, '')
-        let url = `https://api.github.com/repos/${user}/${repo}/zipball`
-        let response = await fetch(url, { method: 'HEAD' })
-        let disposition = response.headers.get('content-disposition')
-        let filename = disposition ? disposition.match(/attachment; filename=(.*)/)[1] : `${repo}.zip`
-        m.reply(wait)
-        await conn.sendMessage(m.chat, { document: { url: url }, mimetype: 'application/zip', fileName: filename.replace('.zip.zip','.zip')}, { quoted : m })
+        let match = args[0].match(regex);
+        let user = match[1];
+        let repo = match[2].replace(/\.git$/, '');
+        let branch = match[3];
+
+        let url = branch 
+            ? `https://api.github.com/repos/${user}/${repo}/zipball/${branch}`
+            : `https://api.github.com/repos/${user}/${repo}/zipball`;
+
+        await m.reply(wait);
+
+        let response = await fetch(url, { method: 'HEAD' });
+        if (response.status === 404) {
+            throw 'Repository atau Branch GitHub tidak ditemukan (404). Pastikan link publik dan benar!';
+        }
+
+        let disposition = response.headers.get('content-disposition');
+        let filename = disposition && disposition.includes('filename=') 
+            ? disposition.split('filename=')[1].replace(/["']/g, '') 
+            : `${repo}${branch ? '-' + branch : ''}.zip`;
+
+        await conn.sendMessage(m.chat, { 
+            document: { url: url }, 
+            mimetype: 'application/zip', 
+            fileName: filename 
+        }, { quoted: m });
+
     } catch (e) {
         if (e !== false) {
             console.log(e);
@@ -21,10 +42,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
         }
     }
 }
-handler.help = ['gitclone <url>']
-handler.tags = ['github']
-handler.command = /gitclone/i
 
-handler.limit = true
+handler.help = ['gitclone <url>'];
+handler.tags = ['github'];
+handler.command = /^gitclone$/i;
+handler.limit = true;
 
 export default handler;
