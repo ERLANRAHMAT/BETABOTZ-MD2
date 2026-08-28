b7e84ea4ccad8ae08ec70e7c85421f6a44408105
dana <danaputra375@gmail.com>
2026-08-28 11:34:51 +0700

    perbaikan ban dan unban user

--- handler.js
+++ handler.js
@@ -64,6 +64,7 @@ export default {
       m.limit = false;
       try {
         let user = global.db.data.users[m.sender];
+        if (user && user.banned) return;
         if (typeof user !== "object") global.db.data.users[m.sender] = {};
         if (user) {
           if (!isNumber(user.saldo)) user.saldo = 0;
