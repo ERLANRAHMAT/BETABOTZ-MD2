788b5b02ba4ca3a0f0baec360c909b990678d192
dana <danaputra375@gmail.com>
2026-08-29 22:17:06 +0700

    nda jadi

--- plugins/group-warning.js
+++ plugins/group-warning.js
@@ -1,46 +1,36 @@
-let war = global.maxwarn 
-
+let war = global.maxwarn
 let handler = async (m, { conn, text, args, groupMetadata, usedPrefix, command }) => {      
-    let who;
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
-    else who = m.chat;
-    
-    if (!who) throw `✳️ Berikan tag atau balas pesan seseorang\n\n📌 Contoh : ${usedPrefix + command} @user`;
-    
-    if (!global.db.data.users[who]) {
-        global.db.data.users[who] = { warn: 0 };
-    }
-    if (typeof global.db.data.users[who].warn !== 'number') {
-        global.db.data.users[who].warn = 0;
-    }
-
-    let warn = global.db.data.users[who].warn;
-    
-    if (warn < war) {
-        global.db.data.users[who].warn += 1;
-        m.reply(`
+        let who
+        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+        else who = m.chat
+        if (!who) throw `✳️ Memberi label atau menyebut seseorang\n\n📌 Contoh : ${usedPrefix + command} @user`
+        if (!(who in global.db.data.users)) throw `✳️ Pengguna hilang dari database saya`
+        let name = await conn.getName(m.sender);
+        let warn = global.db.data.users[who].warn
+        if (warn < war) {
+            global.db.data.users[who].warn += 1
+            m.reply(`
 ⚠️ *Pengguna yang Diperingatkan* ⚠️
 
-▢ *Pengguna:* @${who.split('@')[0]}
-▢ *Memperingatkan:* ${global.db.data.users[who].warn}/${war}
-▢ *Alasan:* ${text || 'Tidak ada alasan'}`, null, { mentions: [who] }); 
-    } else {
-        global.db.data.users[who].warn = 0;
-        m.reply(`⛔ Pengguna telah mencapai batas maksimal peringatan *${war}* dan akan dikeluarkan dari grup.`);
-        await time(3000);
-        await conn.groupParticipantsUpdate(m.chat, [who], 'remove').catch(() => {});
-    }
+▢ *Pengguna:* @${who.split`@`[0]}
+▢ *Memperingatkan:* ${warn + 1}/${war}
+▢ *Alasan:* ${text}`, null, { mentions: [who] }) 
+        } else if (warn == war) {
+            global.db.data.users[who].warn = 0
+            m.reply(`⛔ Pengguna melebihi peringatan *${war}* karena itu akan dihapus`)
+            await time(3000)
+            await conn.groupParticipantsUpdate(m.chat, [who], 'remove')
+        }
 }
+handler.help = ['warn @user']
+handler.tags = ['group']
+handler.command = ['warn'] 
+handler.group = true
+handler.admin = true
+handler.botAdmin = true
 
-handler.help = ['warn @user'];
-handler.tags = ['group'];
-handler.command = ['warn']; 
-handler.group = true;
-handler.admin = true;
-handler.botAdmin = true;
-
-export default handler;
+export default handler
 
 const time = async (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-};
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
