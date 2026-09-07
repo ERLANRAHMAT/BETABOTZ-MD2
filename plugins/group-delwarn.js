8c42254eba520f6ac1a4ea4435bb270446940bea
dana <danaputra375@gmail.com>
2026-09-07 13:20:03 +0700

    fix some issue

--- plugins/group-delwarn.js
+++ plugins/group-delwarn.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, args, groupMetadata}) => {
         let who
         if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
         else who = m.chat
-        if (!who) throw `✳️ Memberi label atau menyebut seseorang`
+        if (!who) throw `Tag sesesorang untuk mengurangi peringatan nya`
         if (!(who in global.db.data.users)) throw `✳️ Pengguna hilang dari database saya`
        let warn = global.db.data.users[who].warn
        if (warn > 0) {
@@ -11,7 +11,6 @@ let handler = async (m, { conn, args, groupMetadata}) => {
          
 ▢ Memperingatkan: *-1*
 ▢ Total Memperingatkan: *${warn - 1}*`)
-         m.reply(`✳️ Seorang admin mengurangi peringatannya, sekarang Anda memiliki *${warn - 1}*`, who)
          } else if (warn == 0) {
             m.reply('✳️ Pengguna tidak memiliki peringatan')
         }
--- plugins/image-anime.js
+++ plugins/image-anime.js
@@ -10,8 +10,8 @@ var handler = async (m, { conn, args, usedPrefix, command }) => {
       throw e;
     }
 }
-handler.help = ['umaru','kaneki','megumin','yotsuba','shinomiya','yumeko','tejina','chiho','toukachan','akira','itori','kurumi','sagiri','eba','deidara','itachi','madara','asuna','ayuzawa','chitoge','emilia','hestia','inori','ana','miku','kaori','shizuka','doraemon','kaga','kotori','mikasa','akiyama','gremory','isuzu','shina','kagura','shinka','tsunade','sasuke','sakura','rize','nezuko','boruto','naruto','erza','minato','elaina','yuri','shota','waifu','loli','hinata']
-handler.command = /^(umaru|keneki|megumin|yotsuba|shinomiya|yumeko|tejina|chiho|toukachan|akira|itori|kurumi|sagiri|eba|deidara|itachi|madara|asuna|ayuzawa|chitoge|emilia|hestia|inori|ana|miku|kaori|shizuka|doraemon|kaga|koturi|mikasa|akiyama|gremory|isuzu|shina|kagura|shinka|tsunade|sasuke|sakura|rize|nezuko|boruto|naruto|erza|minato|elaina|yuri|shota|waifu|loli|hinata)$/i
+handler.help = ['umaru','kaneki','megumin','yotsuba','shinomiya','yumeko','tejina','chiho','toukachan','akira','itori','kurumi','sagiri','eba','deidara','itachi','madara','asuna','ayuzawa','chitoge','emilia','hestia','inori','ana','miku','kaori','shizuka','doraemon','kaga','kotori','mikasa','akiyama','gremory','isuzu','shina','kagura','shinka','tsunade','sasuke','sakura','rize','nezuko','boruto','naruto','erza','minato','elaina','shota','waifu','loli','hinata']
+handler.command = /^(umaru|keneki|megumin|yotsuba|shinomiya|yumeko|tejina|chiho|toukachan|akira|itori|kurumi|sagiri|eba|deidara|itachi|madara|asuna|ayuzawa|chitoge|emilia|hestia|inori|ana|miku|kaori|shizuka|doraemon|kaga|koturi|mikasa|akiyama|gremory|isuzu|shina|kagura|shinka|tsunade|sasuke|sakura|rize|nezuko|boruto|naruto|erza|minato|elaina|shota|waifu|loli|hinata)$/i
 handler.tags = ['image']
 handler.limit = true;
 export default handler;
